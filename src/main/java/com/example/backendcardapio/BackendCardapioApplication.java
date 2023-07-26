c4bd210522ead47a9013bc2f832548ee451e0da6
Rodrigo Barbosa <rdg4368@gmail.com>
2023-07-26 14:10:29 -0300

    add estrutura

---
+++ src/main/java/com/example/backendcardapio/BackendCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCardapioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendcardapio/BackendCardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
